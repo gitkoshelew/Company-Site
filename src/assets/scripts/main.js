18738061324103c4fd510edff1ebd1375ba09ac1
Dzmitry_Koshaleu <d.koshelev@arateg.by>
2019-04-05 19:32:05 +0300

    buttons

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -38,7 +38,21 @@ jQuery(function() {
       ],
     });
 
-    // headerSlider.find('.slick-arrow').html('<div class="round-btn-bg"></div>');
+    const numbers = headerSlider
+      .find('.slick-dots .slick-active button')
+      .attr('aria-label')
+      .split(' ');
+    headerSlider.append('<div class="header__slider-number">1/' + numbers[2] + '</div>');
+
+    headerSlider.on('afterChange', function(event, slick, currentSlide, nextSlide) {
+      const button = $(this).find('.slick-dots .slick-active button');
+      const numbers = $(button)
+        .attr('aria-label')
+        .split(' ');
+      $(this)
+        .find('.header__slider-number')
+        .html(numbers[0] + '/' + numbers[2]);
+    });
   }
 
   if (jQuery('.portfolio__slider-wrapper').length !== 0) {
